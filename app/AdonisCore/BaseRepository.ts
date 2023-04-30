63b6b7eaa75f4ab92fff01dd7f061283c6d0db4e
phong.lg <phong2018@gmail.com>
2023-04-30 09:54:34 +0700

    013: add using RepositoryInterface

--- app/AdonisCore/BaseRepository.ts
+++ app/AdonisCore/BaseRepository.ts
@@ -1,30 +1,67 @@
-export default class BaseRepository {
+import RepositoryInterface from 'App/AdonisCore/Contracts/RepositoryInterface'
+
+export default class BaseRepository implements RepositoryInterface {
   public model
 
-  async create (data) {
+  async filter(filters) {
+    const criteria = new FilterCriteria(filters);
+    criteria.apply(this.model);
+    return this;
+  }
+
+  async find(id: any, columns?: any[]) {
+    return this.model.findOrFail(id,columns)
+  }
+
+  async first(columns?: any[]) {
+    return this.model.first(columns)
+  }
+
+  async firstWhere(where: any, columns?: any[]) {
+    return [where, columns]
+  }
+
+  async firstOrFailWhere(where: any, columns?: any[]) {
+    return [where, columns]
+  }
+
+  async all(columns?: any[]) {
+    return this.model.all(columns)
+  }
+
+  async paginate(limit?: number, columns?: any[]) {
+    return [limit, columns]
+  }
+
+  async simplePaginate(limit?: number, columns?: any[]) {
+    return [limit, columns]
+  }
+
+  async findByField(field: string, value: any, columns?: any[]) {
+    return [field, value, columns]
+  }
+
+  async findWhereIn(field: string, value: any[], columns?: any[]) {
+    return [field, value, columns]
+  }
+
+  async create(data: any) {
     return this.model.create(data)
   }
 
-  async update (id, data) {
-    const record = await this.getById(id)
-    record.merge(data)
+  async update(id: any, attributes: any) {
+    const record = await this.find(id)
+    record.merge(attributes)
     return record.save()
   }
 
-  async delete (id) {
-    const record = await this.getById(id)
+  async delete(id: any) {
+    const record = await this.find(id)
     return record.delete()
   }
 
-  async getById (id) {
-    return this.model.findOrFail(id)
-  }
-
-  async getAll () {
-    return this.model.all()
-  }
-
-  async getByField (field, value) {
-    return this.model.query().where(field, value).fetch()
+  async insert(values: any[]) {
+    console.log(values)
+    return true
   }
 }
---
+++ app/AdonisCore/Contracts/RepositoryInterface.ts
@@ -0,0 +1,100 @@
+export default interface RepositoryInterface {
+  /**
+   * Find record by id
+   *
+   * @param id
+   * @param columns
+   */
+  find(id: any, columns?: any[]): Promise<any>;
+
+  /**
+   * Get first record of repository
+   *
+   * @param columns
+   */
+  first(columns?: any[]): Promise<any>;
+
+  /**
+   * Retrieve first data by multiple fields
+   *
+   * @param where
+   * @param columns
+   */
+  firstWhere(where: any, columns?: any[]): Promise<any>;
+
+  /**
+   * Retrieve first or fail data by multiple fields
+   *
+   * @param where
+   * @param columns
+   */
+  firstOrFailWhere(where: any, columns?: any[]): Promise<any>;
+
+  /**
+   * Get all data of repository
+   *
+   * @param columns
+   */
+  all(columns?: any[]): Promise<any>;
+
+  /**
+   * Get data of repository by pagination
+   *
+   * @param limit
+   * @param columns
+   */
+  paginate(limit?: number, columns?: any[]): Promise<any>;
+
+  /**
+   * Get data of repository by pagination
+   *
+   * @param limit
+   * @param columns
+   */
+  simplePaginate(limit?: number, columns?: any[]): Promise<any>;
+
+  /**
+   * Get all data of repository by field
+   *
+   * @param field
+   * @param value
+   * @param columns
+   */
+  findByField(field: string, value: any, columns?: any[]): Promise<any>;
+
+  /**
+   * Get all data of repository by condition
+   *
+   * @param columns
+   */
+  findWhereIn(field: string, value: any[], columns?: any[]): Promise<any>;
+
+  /**
+   * Create new model
+   *
+   * @param attributes
+   */
+  create(attributes: any): Promise<any>;
+
+  /**
+   * Update the existed model
+   *
+   * @param id
+   * @param attributes
+   */
+  update(id: any, attributes: any): Promise<any>;
+
+  /**
+   * Remove the existed model
+   *
+   * @param id
+   */
+  delete(id: any): Promise<boolean>;
+
+  /**
+   * Insert new records
+   *
+   * @param values
+   */
+  insert(values: any[]): Promise<boolean>;
+}
